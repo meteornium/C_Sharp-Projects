ca6b92bc15dd2ad61fc036714597d69248f62b9b
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-21 16:51:22 -0500

    c# tutorial

---
+++ Basic C# Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!" + "\n" + "How are you?" + "\n" + "Welcome to Acme Accounting Systems" + "\n" + "Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
