97a35cb941c0b46081e118a86baf625b12a8d2a3
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-21 19:37:49 -0500

    comments and added ending
    added comments and closing application

--- Basic C# Programs/basic program/DailyReport.cs
+++ Basic C# Programs/basic program/DailyReport.cs
@@ -10,33 +10,31 @@ namespace VariablesAndDataTypes
     {
         static void Main(string[] args)
         {
-            // prints tech academy and student dail report on a seperate line
-            Console.WriteLine("The Tech Academy" + "\n" + "Student Daily Report");
-            Console.ReadLine();
+            // this is a simple program!!
 
-            // asks name, allows user to input name and saves that as a string variable 
-            Console.WriteLine("What is your name?");
-            string yourName = Console.ReadLine();
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
 
-            // asks course, user inputs course name and saves to a string variable
-            Console.WriteLine("What course are you on?");
-            string courseName = Console.ReadLine();
+            //Console.WriteLine("What is your age?");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You are " + age + " years old.");
+            //Console.ReadLine();
 
-            //asks page number, saves that as an integer variable
-            Console.WriteLine("What page number?");
-            int pageNum = Convert.ToInt32(Console.ReadLine());
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //char questionMark = '\u2103';
 
-            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
-            bool needHelp = Console.ReadLine();
+            //decimal money = 100.5m;
 
-            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
-            string posExp = Console.ReadLine();
+            //float seconds = 2.62f;
 
-            Console.WriteLine("Is there arny other feedback you'd like to provide? Please be specific.");
-            string posExp = Console.ReadLine();
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
 
-            Console.WriteLine("How many hours did you study today?");
-            int hoursNum = Convert.ToInt32(Console.ReadLine());
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
 
 
         }
--- Basic C# Programs/basic program/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ Basic C# Programs/basic program/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -26,17 +26,28 @@ namespace VariablesAndDataTypes
             Console.WriteLine("What page number?");
             int pageNum = Convert.ToInt32(Console.ReadLine());
 
+            // asks if need help, makes boolean variable
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
             bool needHelp = Convert.ToBoolean(Console.ReadLine());
 
+            // asks about positive experiences, saves as a string variable
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string posExp = Console.ReadLine();
 
+            // asks if youd like to leave feedback, saves response as a string variable
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string feedPro = Console.ReadLine();
 
+            // asks how many hours studied, saves as an integer
             Console.WriteLine("How many hours did you study today? Please use only the number.");
             int hoursNum = Convert.ToInt32(Console.ReadLine());
+
+            // prints final line
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+            // exits
+            System.Environment.Exit(1);
         }
     }
 }
