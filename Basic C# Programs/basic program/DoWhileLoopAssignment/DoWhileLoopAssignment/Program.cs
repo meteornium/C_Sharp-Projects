4e55df5c63124015aa20425e22b220d1a7250d7c
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-22 16:32:43 -0500

    do while loop assignment

--- Basic C# Programs/basic program/DoWhileLoopAssignment/DoWhileLoopAssignment/Program.cs
+++ Basic C# Programs/basic program/DoWhileLoopAssignment/DoWhileLoopAssignment/Program.cs
@@ -10,6 +10,76 @@ namespace DoWhileLoopAssignment
     {
         static void Main(string[] args)
         {
+            // create variable color thatn takes user input for comparison against future cases in switch statement. creates a boolean in order for us to perform loop
+            Console.WriteLine("Guess my favorite color!");
+            string color = Console.ReadLine();
+            bool isGuessed = color == "green";
+
+            // beginning of do while loop. switch statement inside
+            do
+            {
+                switch (color)
+                {
+                    case "blue":
+                        Console.WriteLine("Blue is NOT my favorite color!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "red":
+                        Console.WriteLine("Red is NOT my favorite color!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "black":
+                        Console.WriteLine("Black is NOT my favorite color!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("Green IS my favorite color! You win!");
+                        isGuessed = true; // setting isGuessed to true in order to exit the loop
+                        break;
+                    default:
+                        Console.WriteLine("That is NOT my favorite color!");
+                        Console.WriteLine("Guess my favorite color!");
+                        color = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed); // states that while isGuessed is false, do this loop
+
+            //while (!isGuessed) // while isGuessed is false, this will happen
+            //{
+            //    switch (color)
+            //    {
+            //        case "blue":
+            //            Console.WriteLine("Blue is NOT my favorite color!");
+            //            Console.WriteLine("Guess my favorite color!");
+            //            color = Console.ReadLine();
+            //            break;
+            //        case "red":
+            //            Console.WriteLine("Red is NOT my favorite color!");
+            //            Console.WriteLine("Guess my favorite color!");
+            //            color = Console.ReadLine();
+            //            break;
+            //        case "black":
+            //            Console.WriteLine("Black is NOT my favorite color!");
+            //            Console.WriteLine("Guess my favorite color!");
+            //            color = Console.ReadLine();
+            //            break;
+            //        case "green":
+            //            Console.WriteLine("Green IS my favorite color! You win!");
+            //            isGuessed = true; // lets us exit our loop
+            //            break;
+            //        default:
+            //            Console.WriteLine("That is NOT my favorite color!");
+            //            Console.WriteLine("Guess my favorite color!");
+            //            color = Console.ReadLine();
+            //            break;
+            //    }
+            //}
+
+            Console.Read();
         }
     }
 }
