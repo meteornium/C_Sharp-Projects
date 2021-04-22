e3fa2d5ac419fb734a1fe38f59f30d6d16b71682
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-22 11:21:42 -0500

    user input operations assignment

---
+++ Basic C# Programs/basic program/MathAndCompaisonOperators/MathAndCompaisonOperators/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndCompaisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 5 - 3;
+            //Console.WriteLine("Five minus three = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int multiply = 10 * 5;
+            //Console.WriteLine("Ten times five = " + multiply.ToString());
+            //Console.ReadLine();
+
+            //int dividend = 200 / 3;
+            //Console.WriteLine(dividend);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            //bool isWarm = currentTemp >= roomTemp;
+            //bool isWarm = currentTemp == roomTemp;
+            bool isWarm = currentTemp != roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic C# Programs/basic program/UserInputOperations/UserInputOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Submit a number.");
+            int multiply = Convert.ToInt32(Console.ReadLine());
+            int product = multiply * 50;
+
+            Console.WriteLine("Your number times 50 is..." + product.ToString());
+
+            Console.WriteLine("Submit a number.");
+            int add = Convert.ToInt32(Console.ReadLine());
+            int result = add + 25;
+
+            Console.WriteLine("Your number plus 25 is..." + result.ToString());
+
+            Console.WriteLine("Submit a number.");
+            double divide = Convert.ToInt32(Console.ReadLine());
+            double quotient = divide / 12.5;
+
+            Console.WriteLine("Your number divided by 12.5 is..." + quotient.ToString());
+
+            Console.WriteLine("Submit a number.");
+            int compare = Convert.ToInt32(Console.ReadLine());
+            bool comparison = compare > 50;
+
+            Console.WriteLine("Your number is greater than 50..." + comparison.ToString());
+
+            Console.WriteLine("Submit a number");
+            int divider = Convert.ToInt32(Console.ReadLine());
+            double modulus = divider % 7;
+
+            Console.WriteLine("The remainder of your number divided by 7 is..." + modulus.ToString());
+            Console.ReadLine();
+
+            System.Environment.Exit(1);
+        }
+    }
+}
