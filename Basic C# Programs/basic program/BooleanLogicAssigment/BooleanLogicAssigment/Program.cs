ab8945d18b7969f729cbd8fad5658101745cd352
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-22 12:42:48 -0500

    Boolean logic Assignment

---
+++ Basic C# Programs/basic program/BooleanLogicAssigment/BooleanLogicAssigment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+           int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Answer \"true\" or \"false\".");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool qualified = (age > 15 && dui == false && tickets <= 3);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+            System.Environment.Exit(1);
+        }
+    }
+}
