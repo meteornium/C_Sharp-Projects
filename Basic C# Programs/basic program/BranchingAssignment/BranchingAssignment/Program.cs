0022858daf58b37d3aa90026e8057972820cee2d
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-22 14:10:06 -0500

    Branching Assignment

---
+++ Basic C# Programs/basic program/BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below." + "\n" + "Enter package weight as a number:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package to heavy to be shipped via Pacakge express. Have a good day.");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+            }
+
+            Console.WriteLine("Enter package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            decimal quote = ((width * height * length) * weight) / 100m;
+
+            Console.WriteLine("Your estimated total for shipping this package is: $" + quote + "\n" + "Thank you!" + "\n" + "Press any key to exit");
+            Console.ReadLine();
+
+            System.Environment.Exit(1);
+        }
+    }
+}
