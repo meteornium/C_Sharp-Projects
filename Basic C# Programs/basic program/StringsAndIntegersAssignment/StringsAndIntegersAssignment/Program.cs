07bf2a8c386f77b5061225769169a6c695374912
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-23 10:57:20 -0500

    Strings and Integers Assignment

---
+++ Basic C# Programs/basic program/StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>() { 100, 200, 300, 400, 500 };
+            List<int> modifiedList = new List<int>(); // empty list to be used to push numbers in
+
+            try
+            {
+                Console.WriteLine("Give a number and I will divide everything in my list by that number!");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                // loop going through numberList, dividing each element by userNumber and then adding them to the empty modifiedList
+                for (int i = 0; i < numberList.Count; i++)
+                {
+                    modifiedList.Add(numberList[i] / userNumber);
+                }
+
+                // this foreach prints each element in modifiedList, showing the results
+                foreach (int number in modifiedList)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            catch (FormatException) // if you input something other than an int
+            {
+                Console.WriteLine("Please input a number.");
+            }
+            catch (DivideByZeroException) // if user inputs 0
+            {
+                Console.WriteLine("You cannot divide by zero.");
+            }
+            catch (Exception ex) // all other cases
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
