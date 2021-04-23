231da42812361540513054f7660fd0fd86c3a7a3
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-23 15:03:05 -0500

    Method Overloading Assignment

---
+++ Basic C# Programs/basic program/MainMethodAssignment/MainMethodAssignment/Operation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Operation
+    {
+        public int Addition(int operate)
+        {
+            Random random = new Random();
+            int result = random.Next(1, 1000) + operate;
+            return result;
+        }
+
+        public int Addition(decimal operate)
+        {
+            Random random = new Random();
+            int operate2 = Convert.ToInt32(operate);
+            int result = random.Next(500, 1500) + operate2;
+            return result;
+        }
+        public int Addition(string operate)
+        {
+            Random random = new Random();
+            int operate2 = Convert.ToInt32(operate);
+            int result = random.Next(1000, 2000) + operate2;
+            return result;
+        }
+    }
+}
---
+++ Basic C# Programs/basic program/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation instance = new Operation();
+
+            Console.WriteLine("Give a number.");
+            int userNumberOne = Convert.ToInt32(Console.ReadLine());
+            
+            int resultOne = instance.Addition(userNumberOne);
+            Console.WriteLine("Your result is: " + resultOne);
+            Console.ReadLine();
+
+            Console.WriteLine("Give a decimal.");
+            decimal userNumberTwo = Convert.ToDecimal(Console.ReadLine());
+
+            int resultTwo = instance.Addition(userNumberTwo);
+            Console.WriteLine("Your result is: " + resultTwo);
+            Console.ReadLine();
+
+            Console.WriteLine("Give a number.");
+            string userNumberThree = Console.ReadLine();
+
+            int resultThree = instance.Addition(userNumberThree);
+            Console.WriteLine("Your result is: " + resultThree);
+            Console.ReadLine();
+        }
+    }
+}
