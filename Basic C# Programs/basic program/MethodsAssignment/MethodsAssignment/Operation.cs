e1a0c3c1c97d83d4c6b8fa944d7bf383c9f7a5fc
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-23 12:58:48 -0500

    calling methods assignment

---
+++ Basic C# Programs/basic program/MethodsAssignment/MethodsAssignment/Operation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Operation
+    {
+        //multipy, subtract, add methods. take user input and use math.random
+        public static int Add(int operation)
+        { // adds the number by a random num
+            Random random = new Random(); // this instatiates random
+            int result = operation + random.Next(1, 1000); // limiting the random num between 1 and 1000
+            return result; // returns the result
+        }
+
+        public static int Multiply(int operation)
+        { // multiplies by a random num
+            Random random = new Random();
+            int result = operation * random.Next(1, 1000);
+            return result;
+        }
+
+        public static int Subtract(int operation)
+        {
+            Random random = new Random();
+            int result = operation - random.Next(1, 1000);
+            return result;
+        }
+    }
+}
--- Basic C# Programs/basic program/MethodsAssignment/MethodsAssignment/Program.cs
+++ Basic C# Programs/basic program/MethodsAssignment/MethodsAssignment/Program.cs
@@ -10,6 +10,16 @@ namespace MethodsAssignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Submit a number to do math operations on.");
+            int userNum = Convert.ToInt32(Console.ReadLine()); // turning user input into an int to be used in the methods
+
+            Console.WriteLine("Added to a random number: " + Operation.Add(userNum)); // calling method
+
+            Console.WriteLine("Multiplied by a random number: " + Operation.Multiply(userNum));
+
+            Console.WriteLine("Subtracted by a random number: " + Operation.Subtract(userNum));
+
+            Console.ReadLine();
         }
     }
 }
