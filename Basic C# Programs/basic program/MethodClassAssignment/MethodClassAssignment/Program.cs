e0368bd4bcc7ea5d97d5ca5aa7f828bf2517787b
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-23 15:45:19 -0500

    Void Method Assignment

---
+++ Basic C# Programs/basic program/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidClass instance = new VoidClass();
+
+            // calling object and applying method to it
+            instance.VoidMethod(21, 32);            
+
+            // using parameter names when calling method on object
+            instance.VoidMethod(first: 33, second: 50);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic C# Programs/basic program/MethodClassAssignment/MethodClassAssignment/VoidClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class VoidClass
+    {
+        public void VoidMethod(int first, int second)
+        {
+            // math done on the first parameter
+            int firstMessedWith = first * 900 / 64 + 94 - 333 + 853 * 7 / 90 % 8;
+
+            // simple writeline for second parameter
+            Console.WriteLine(second);
+        }
+    }
+}
