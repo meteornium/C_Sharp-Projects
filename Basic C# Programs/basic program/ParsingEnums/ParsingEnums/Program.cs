2bc2dbd7c121431f8a4f5bae9165983f40473145
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-26 11:45:11 -0500

    Parsing Enums Assignment

---
+++ Basic C# Programs/basic program/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                string userInput = Console.ReadLine();
+                DaysOfTheWeek daysOf = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput, true);
+                Console.WriteLine("You entered: " + daysOf);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
--- Basic C# Programs/basic program/TwentyOne/TwentyOne/Program.cs
+++ Basic C# Programs/basic program/TwentyOne/TwentyOne/Program.cs
@@ -10,17 +10,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Adam", "Casey", "Mike", "Danny", "Kyle" };
-            //game.ListPlayers();
-            //Console.ReadLine();
-
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Adam";
-            game += player;
-
             Deck deck = new Deck();
             deck.Shuffle(4);
 
