d3af5a3806416a1df1118afb7232c63b5cba7dd4
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-27 10:56:45 -0500

    DateTime Assignment

---
+++ Basic C# Programs/basic program/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime rightNow = DateTime.Now; // setting the current time to a variable
+            Console.WriteLine(rightNow); // this prints the current datetime to the console
+            Console.WriteLine("Input a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine()); // taking in user input as an int
+            DateTime futureTime = rightNow.AddHours(userNum); // setting new variable equal to the current time plus the user inputted num in terms of hours
+
+            Console.WriteLine("In {0} hours, it will be {1}.", userNum, futureTime); // printing the result
+            Console.ReadLine();
+        }
+    }
+}
