e858437c48fb798cf39a5b1c79fc1c302a794eb4
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-27 13:22:59 -0500

    Constructor Assignment

---
+++ Basic C# Programs/basic program/ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Person
+    {
+        // connecting the larger constructor to this on, setting income at 50000 if nothing is inputted
+        public Person(string name) : this(name, 50000)
+        {
+        }
+
+        public Person(string name, int income)
+        {
+            Name = name;
+            Income = income;
+        }
+
+        public string Name { get; set; } 
+        public int Income { get; set; }
+    }
+}
---
+++ Basic C# Programs/basic program/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const to set name of company, so that it cannot be changed
+            const string companyName = "Angler Bait & Tackle";
+
+            // using var here because you can still tell that this is a lisst variable type
+            var personList = new List<string>();
+        }
+    }
+}
