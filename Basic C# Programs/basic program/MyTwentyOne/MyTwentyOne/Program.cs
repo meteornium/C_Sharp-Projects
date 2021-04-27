223e4462c64ee1e32b03fc680a09d7c6b813be24
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-27 13:24:03 -0500

    Casino and Remake

---
+++ Basic C# Programs/basic program/MyTwentyOne/MyTwentyOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Game Corner. Please tell me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "ya" || answer == "y")
+            {
+                Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\adamk\logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thanks for playing!");
+            }
+            Console.WriteLine("Feel free to look around. Bye for now.");
+            Console.ReadLine();
+        }
+    }
+}
