a3d0be649afcb68678a65d4aacf9d733e7e542d3
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-05-01 10:42:03 -0500

    Code First Assignment (CodeFirstChallenge)

---
+++ Basic C# Programs/basic program/CodeFirstChallenge/CodeFirstChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student1 = new Student() { StudentName = "Adam" };
+
+                ctx.Students.Add(student1);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
